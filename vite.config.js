c9da6920da7e4b9c1a349a54efb7ba76c341d1c5
katosamuelraymond <katosamuelraymondmarvinhosborn@gmail.com>
2025-11-13 09:39:28 +0300

    worked on the exam module....where teachers and admins can create ,edit,view exams in both the question bank and also custom questions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
+
 
 export default defineConfig({
   plugins: [
@@ -11,7 +11,7 @@ export default defineConfig({
       ],
       refresh: true,
     }),
-    // tailwindcss(),
+
   ],
   resolve: {
     alias: {
